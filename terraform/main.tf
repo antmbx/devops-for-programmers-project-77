b22ee81b5ad97f9ed9980535d76aeef4d8ac1606
Anton Dmitriev <antmbx@gmail.com>
2024-08-11 02:42:41 +0000

    path to key

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,7 @@ resource "yandex_compute_instance" "default" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    ssh-keys = "ubuntu:${file("/home/ubuntu/.ssh/id_rsa.pub")}"
     #user-data = "${file("./cloud-init.yml")}"
     user-data = <<-EOF
     #!/bin/bash
