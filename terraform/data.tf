3dc22495a88dc82d331fa735541a1376d7da1ee1
Anton Dmitriev <antmbx@gmail.com>
2024-08-18 12:00:39 +0000

    depends var on yandex_compute_instance

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,6 +7,7 @@ data "yandex_mdb_postgresql_cluster" "dbcluster" {
 data "yandex_compute_instance" "default" {
   name      = var.lb-name
   folder_id = var.folderID
+  depends_on = [yandex_compute_instance.default]
 }
 
 output "instance_external_ip" {
